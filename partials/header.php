d8be9daa316b307af0aba93faddb4704921d259d
Aleksandar Pavlovic <aleksandar84pavlovic@yahoo.com>
2024-05-18 00:53:15 +0200

    Small fixes, partials contain imgs, sql dump added

--- partials/header.php
+++ partials/header.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href=" https://cdn.jsdelivr.net/npm/sweetalert2@11.11.0/dist/sweetalert2.min.css " rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="fav-icon.jpeg">
+    <link rel="icon" type="image/x-icon" href="partials/fav-icon.jpeg">
 </head>
 <body>
 
     <nav class="navbar navbar-dark bg-dark text-light sticky-top p-1 mb-5" style="background-color: #e3f2fd;">
         <a class="navbar-brand" href="https://www.paragraf.rs">
-            <img src="p-lex-logo.png" width="150" height="50" class="d-inline-block align-left" alt="">
+            <img src="partials/p-lex-logo.png" width="150" height="50" class="d-inline-block align-left" alt="">
             
         </a>
         <div class="d-flex">
